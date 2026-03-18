a320eafb1d69c00912ea1a6a4d6a5b34f1e2e1b0
priyanka03s <priyankayadhav1720@gmail.com>
2026-03-18 17:19:07 +0530

    add

--- Server.js
+++ Server.js
@@ -39,7 +39,8 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://localhost:3000',
   'https://green-inovics-002.netlify.app',
-  "https://green-inovics-web.netlify.app"
+  "https://green-inovics-web.netlify.app",
+  "https://reliable-torrone-279885.netlify.app/"
 ];
 
 if (process.env.ALLOWED_ORIGINS) {
